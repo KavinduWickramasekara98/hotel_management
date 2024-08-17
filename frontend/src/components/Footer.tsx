54cfb3a7180dd31154119de161f5f643ba144eb9
Kavindu Wickramasekara <kavi2020wick@gmail.com>
2024-08-17 21:53:37 +0530

    initial commit

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,17 @@
 const Footer = ()=>{
     return (
-        <div className="bg-blue-800 py-10">
-            <div className="container mx-auto flex justify-between items-center">
-                <span className="text-3xl text-white font-bold tracking-tight"></span>
-            </div>
+      <div className="bg-blue-800 py-10">
+        <div className="container mx-auto flex justify-between items-center">
+          <span className="text-3xl text-white font-bold tracking-tight">
+            HotelCalifornia.com
+          </span>
+          <span className="text-white font-bold tracking-tight flex gap-4">
+            <p className="cursor-pointer">privacy Plicy</p>
+            <p className="cursor-pointer">Terms of Service</p>
+            <p className="cursor-pointer">privacy Plicy</p>
+          </span>
         </div>
-    )
+      </div>
+    );
 }
+export default Footer;
